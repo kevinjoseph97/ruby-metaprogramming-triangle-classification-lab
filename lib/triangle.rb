a1bca3cba399fb0fbb5d20db392094253593a9ee
Kevin <kevinjoseph97@gmail.com>
2021-02-04 00:53:44 -0500

    commit tree a14d7f5501c9716e37c8f8e399134f044e0d86bd

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+
+  attr_reader :side1, :side2, :side3
+
+  
+
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    if !self.triangle
+      raise TriangleError
+    elsif side1 == side2 && side2 == side3
+      return :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+        return :isosceles
+    else
+      return :scalene
+    end
+
+    
+  end
+
+  def triangle
+    return side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 + side2 && 
+    side1 > 0 && side2 > 0 && side3 > 0
+  end
+
+
+
+  class TriangleError < StandardError
+    def message
+      "That is not going to work"
+    end
+  end
+
+   
+    
 end
